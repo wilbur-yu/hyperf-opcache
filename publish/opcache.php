3680ce07115ef31c2ab313e8ce76693559334c0b
wilbur.yu <ziyanziyu77@gmail.com>
2021-01-12 22:40:00 +0800

    add config file

---
+++ publish/opcache.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  wenber.yu@creative-life.club
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'directories' => [
+        BASE_PATH . '/app',
+        BASE_PATH . '/bin',
+        BASE_PATH . '/config',
+        BASE_PATH . '/runtime/container',
+        BASE_PATH . '/storage',
+        BASE_PATH . '/vendor',
+    ],
+    'exclude_dirs' => [
+        'test',
+        'Test',
+        'tests',
+        'Tests',
+        'stub',
+        'Stub',
+        'stubs',
+        'Stubs',
+        'dumper',
+        'Dumper',
+        'Autoload',
+        'swoole',
+        'jetbrains',
+        'symfony/polyfill-intl-idn',
+    ],
+
+    'exclude_files' => [
+        'bootstrap80.php',
+    ],
+];
